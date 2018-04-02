c657bee8b487b4f273ed50f7b172db56c3f6548e
curlyz <curly.saigonese@gmail.com>
2018-04-02 10:52:42 +0700

    Create Blocky.py

---
+++ src/Blocky.py
@@ -0,0 +1,14 @@
+__version__ = 0 
+__authors__ = 'vanminh0910'
+
+# HARDWARE DEFINITION ( Pin1 , Pin2 , ADC )
+Port1 = ( 25 , 26 , 36 )
+Port2 = ( 27 , 14 , 37 )
+Port3 = ( 13 , 15 , 38 )
+Port4 = ( 04 , 16 , 39 )
+Port5 = ( 32 , 17 , 32 )
+Port6 = ( 33 , 18 , 33 )
+Port7 = ( 23 , 19 , 34 )
+Port8 = ( 22 , 21 , 35 )
+
+
